86c4f3d227f37db588c275681d79d91fb8c4eb60
IgorGarciaCosta <igonildo7@gmail.com>
2020-10-07 11:34:56 -0300

    First commit. Forward movements and scenery recycle

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private CharacterController controller;
+    public float speed;
+    public float jumpHeigth;
+    public float gravity;
+    private float jumpVelocity;
+    void Start()
+    {
+        //pega o capsule collider do plyer
+        controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //add eixo z (a frente do personagem)
+        Vector3 direction = Vector3.forward * speed;
+
+        if(controller.isGrounded){//saber se está tocando do não
+            //confere se pressionou espaco
+            if(Input.GetKeyDown(KeyCode.Space)){
+                jumpVelocity = jumpHeigth;
+            }
+        }
+
+        else{//se está no ar, decresce a gravidade da vel. do pulo
+            jumpVelocity-=gravity;
+        }
+
+        direction.y = jumpVelocity;
+
+        controller.Move(direction*Time.deltaTime);
+    }
+}
---
+++ Assets/Scripts/SpawnPlatform.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnPlatform : MonoBehaviour
+{
+    //lista que vai guardar as plataformas pra serem carregadas à frente
+    public List<GameObject> platforms = new List<GameObject>();//prefabs
+
+    public List<Transform> currentPlatforms = new List<Transform>();//objetos instanciados
+    public int offset;//distância entre as plataformas
+
+    private Transform player;
+    private Transform currentPlatformPoint;
+    private int platformIndex;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+
+        for(int i=0; i<platforms.Count; i++){
+            //obj, posição e rotação
+            Transform p = Instantiate(platforms[i], new Vector3(0, 0, i*86), transform.rotation).transform;//instancia plataforma
+            currentPlatforms.Add(p);
+            offset +=86;//86 é o tamanho de uma plataforma
+        }
+
+        currentPlatformPoint = currentPlatforms[platformIndex].GetComponent<Platform>().point;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //quando o player passar por cima do ponto, o distance vira 0 e passa a ser positivo
+        float distance = player.position.z - currentPlatformPoint.position.z;
+
+        if(distance>=5){//passou da plataforma
+            //recebe a plataforma que vai ser reposicionada
+            Recycle(currentPlatforms[platformIndex].gameObject);
+            platformIndex++;
+
+            //zera o index antes de chegar em 3, senão dá erro
+            if(platformIndex>currentPlatforms.Count-1){
+                platformIndex = 0;
+            }
+
+            currentPlatformPoint = currentPlatforms[platformIndex].GetComponent<Platform>().point;
+        }
+    }
+
+    public void Recycle(GameObject platform){
+        platform.transform.position = new Vector3(0, 0, offset);
+        offset+=86;
+    }
+}
