a7d564a8518504acfa5276354c542c2ac34bae57
IgorGarciaCosta <igonildo7@gmail.com>
2020-10-08 16:29:51 -0300

    character movements

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -9,6 +9,10 @@ public class Player : MonoBehaviour
     public float jumpHeigth;
     public float gravity;
     private float jumpVelocity;
+
+    public float horizontalSpeed;
+    private bool isMovingLeft;
+    private bool isMovingRight;
     void Start()
     {
         //pega o capsule collider do plyer
@@ -26,6 +30,17 @@ public class Player : MonoBehaviour
             if(Input.GetKeyDown(KeyCode.Space)){
                 jumpVelocity = jumpHeigth;
             }
+
+            if(Input.GetKeyDown(KeyCode.LeftArrow) && transform.position.x > -0.5f && !isMovingLeft){
+                isMovingLeft = true;
+                StartCoroutine(LeftMove());
+            }
+
+            //confere se a tecla foi apertada e se o personagem não chegou ainda ao limite de distância pra não cair
+            if(Input.GetKeyDown(KeyCode.RightArrow) && transform.position.x < 0.5f && !isMovingRight){
+                isMovingRight = true;
+                StartCoroutine(RightMove());
+            }
         }
 
         else{//se está no ar, decresce a gravidade da vel. do pulo
@@ -36,4 +51,26 @@ public class Player : MonoBehaviour
 
         controller.Move(direction*Time.deltaTime);
     }
+
+    //corrotinas para movimentação
+    IEnumerator LeftMove(){
+        
+        for(float i = 0; i<10; i+=0.1f){//100 iterações
+
+            //val de -1 no eixo x
+            controller.Move(Vector3.left*Time.deltaTime*horizontalSpeed);
+            yield return null;
+        }
+        isMovingLeft = false;
+    }
+
+    IEnumerator RightMove(){
+        for(float i=0; i<10; i+=0.1f){
+            //val de 1 no eixo x
+            controller.Move(Vector3.right*Time.deltaTime*horizontalSpeed);
+            yield return null;
+        }
+        isMovingRight = false;
+    }
+    
 }
