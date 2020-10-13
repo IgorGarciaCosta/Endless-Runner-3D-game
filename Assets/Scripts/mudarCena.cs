79afbf265322651881b8e47971284d9bf919670e
IgorGarciaCosta <igonildo7@gmail.com>
2020-10-13 11:18:29 -0300

    menu and buttons

---
+++ Assets/Scripts/mudarCena.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class mudarCena : MonoBehaviour
+{
+
+    public string nomeDaCena;
+    //public AudioClip click;
+    
+    public void ChangeS(){
+        SceneManager.LoadScene(nomeDaCena);
+    }
+
+    public void SairDoJogo(){
+        Application.Quit();
+    }
+}
